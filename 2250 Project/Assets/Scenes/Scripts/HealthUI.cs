bcd39f080c829764ee34a269008f2ed88a256565
alexjnelson <alexj.nelson9@gmail.com>
2020-04-01 18:59:39 -0400

    Added Health Bar Functionality

---
+++ 2250 Project/Assets/Scenes/Scripts/HealthUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    void Update()
+    {
+        GetComponent<Slider>().value = PlayerMovement.instance.gameObject.GetComponent<Health>().health / 100f;
+    }
+}
