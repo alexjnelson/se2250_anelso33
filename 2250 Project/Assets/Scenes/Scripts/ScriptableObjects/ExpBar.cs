2788becb38935fa704fcdcf94ca28f2c579f8cf9
alexjnelson <alexj.nelson9@gmail.com>
2020-04-01 02:56:34 -0400

    Updated Exp Thresholds To Allow for Infinite Level Gain

--- 2250 Project/Assets/Scenes/Scripts/ScriptableObjects/ExpBar.cs
+++ 2250 Project/Assets/Scenes/Scripts/ScriptableObjects/ExpBar.cs
@@ -6,13 +6,21 @@ using UnityEngine;
 public class ExpBar : ScriptableObject
 {
     public int experience = 0;
-    private int[] _thresholds = { 10, 20, 30, 40, 50 };
 
     public void GainExperience(int experience)
     {
-        if ((this.experience += experience) > _thresholds[PlayerMovement.instance.level])
+        if ((this.experience += experience) > GetThreshold(PlayerMovement.instance.level))
         {
             PlayerMovement.instance.LevelUp();
         }
+    }
+
+    /*
+    Recursive algorithm causing the first level to be gained at 4exp, and subsequent levels require an additional 2exp to
+    be gained per level such that the thresholds are { 4, 10, 18, 28, 40, 64 ... } total exp
+    */
+    private int GetThreshold(int level){
+        if (level == 0) { return 4; }
+        else { return 2*level + 4 + GetThreshold(level-1); }
     }
 }
