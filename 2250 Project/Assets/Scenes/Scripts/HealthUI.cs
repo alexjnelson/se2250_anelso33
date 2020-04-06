144001f480d6ae067716d245b21f0f06dfdd9515
alexjnelson <alexj.nelson9@gmail.com>
2020-04-05 20:37:42 -0400

    Added Menu to Forest

--- 2250 Project/Assets/Scenes/Scripts/HealthUI.cs
+++ 2250 Project/Assets/Scenes/Scripts/HealthUI.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 
 public class HealthUI : MonoBehaviour
 {
+    public GameObject levelBox;
     void Update()
     {
         try {
             GetComponent<Slider>().value = PlayerMovement.instance.gameObject.GetComponent<Health>().health / 100f;
+            levelBox.GetComponent<Text>().text = "Levels Completed:" + PlayerMovement.instance.levelsCleared + "/3";
         }
         catch {
-            // yerr
+            
         }
         
     }
